4b85311cd8dbb7e142c478968c98929f263b5aa3
sPyKeRT1 <franciscortc@gmail.com>
2024-09-10 17:36:35 -0300

    update2

--- Tarea1/CrearListas.py
+++ Tarea1/CrearListas.py
@@ -5,7 +5,7 @@ def generar_lista_aleatoria(n):
 
 def generar_lista_semi_ordenada(n):
     lista = sorted([random.randint(0, n) for _ in range(n)])
-    num_desordenados = n // 2
+    num_desordenados = n // 3
     for _ in range(num_desordenados):
         idx1 = random.randint(0, n - 1)
         idx2 = random.randint(0, n - 1)
@@ -18,17 +18,23 @@ def generar_lista_parcialmente_ordenada(n):
     lista_desordenada = [random.randint(0, n) for _ in range(n - punto_corte)]
     return lista_ordenada + lista_desordenada
 
-def crear_archivo_listas(cantidad, minimo, maximo):
-    with open("listas_numeros.txt", "w") as archivo:
-        for i in range(cantidad):
-            n = random.randint(minimo, maximo)
-            if i < (cantidad // 3):
-                lista = generar_lista_aleatoria(n)
-            elif i < ((cantidad * 2) // 3):
-                lista = generar_lista_semi_ordenada(n)
-            else:
-                lista = generar_lista_parcialmente_ordenada(n)
-            
-            archivo.write(" ".join(map(str, lista)) + "\n")
+def crear_archivo_listas(cantidad, minimo, maximo, tipo):
+    with open("listas_numeros.txt", "w") as archivo: 
+        if(tipo == 1):
+            for i in range(cantidad):
+                lista = generar_lista_aleatoria(random.randint(minimo, maximo))
+                archivo.write(" ".join(map(str, lista)) + "\n")
+        elif(tipo == 2):
+            for i in range(cantidad):
+                lista = generar_lista_semi_ordenada(random.randint(minimo, maximo))
+                archivo.write(" ".join(map(str, lista)) + "\n")
+        elif(tipo == 3):
+            for i in range(cantidad):
+                lista = generar_lista_parcialmente_ordenada(random.randint(minimo, maximo))
+                archivo.write(" ".join(map(str, lista)) + "\n")
 
-crear_archivo_listas(15,10,30)
+A = int(input("Ingrese el número de listas: "))
+B = int(input("Ingrese el largo minimo de las listas: "))
+C = int(input("Ingrese el largo maximo de las listas: "))
+D = int(input("Ingrese 1 para Listas Aleatorias, 2 para Semiordenadas o 3 para Parcialmente ordenadas: "))
+crear_archivo_listas(A,B,C,D)
